59e602b34a9d1c137d81e821e3b777b053583c69
suupham2506 <suu.pham2506@gmail.com>
2017-07-07 10:55:01 +0700

    static pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,13 @@ Rails.application.routes.draw do
   root "sessions#new"
   get "/login", to: "sessions#new"
 
-end
 
+  get "/about", to: "static_pages#about"
+
+  get "/help", to: "static_pages#contact_help"
+
+  get "/privacy", to: "static_pages#privacy"
+
+  get "/terms", to: "static_pages#terms"
+
+end
